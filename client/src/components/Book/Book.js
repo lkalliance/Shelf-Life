7bd41eb855952ebe4e0b8553bcf5fa189898591f
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-05-18 16:21:54 -0500

    Added experimental click/double-click handler

--- client/src/components/Book/Book.js
+++ client/src/components/Book/Book.js
@@ -3,6 +3,19 @@ import { Draggable } from "@hello-pangea/dnd";
 import { isTight, abbreviateTitle } from "../../utils/dragUtils";
 
 function Book({ bookId, book, bookIndex }) {
+  let timer;
+  function clickHandler(e) {
+    clearTimeout(timer);
+
+    if (e.detail === 1) {
+      timer = setTimeout(() => {
+        alert(book.title);
+      }, 200);
+    } else if (e.detail === 2) {
+      alert(`Remove ${book.title}`);
+    }
+  }
+
   const textStyle = isTight(book);
   return (
     <Draggable key={bookId} draggableId={bookId} index={bookIndex}>
@@ -15,9 +28,7 @@ function Book({ bookId, book, bookIndex }) {
           <div
             className={`book ${book.color} ${book.thickness} ${book.height} ${book.style} ${textStyle}`}
             id={bookId}
-            onClick={() => {
-              alert(book.title);
-            }}
+            onClick={clickHandler}
           >
             <div className="accent top"></div>
             <div className="spineText">
