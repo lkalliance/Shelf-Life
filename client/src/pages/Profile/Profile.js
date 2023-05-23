5bb98459b30051755f874b7467e96f4bd19b7f3e
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-05-23 15:00:42 -0500

    Removed extra add a book and about links

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,12 +1,9 @@
 import "./Profile.css";
 import { useRecoilState } from "recoil";
-import Auth from "../../utils/auth";
 
 import { userBooksAtom } from "../../recoil/atom/userBooksAtom";
 
 function Profile() {
-  if (!Auth.loggedIn()) window.location.href = "/";
-
   const [books, setBooks] = useRecoilState(userBooksAtom);
 
   return <div>I'm on the profile page</div>;
