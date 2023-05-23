6b51556900cbf89c81d20dae3e93fd9171b029d8
ChapstickLover97 <ashtonbalder@gmail.com>
2023-05-23 15:56:30 -0500

    Committing starter home files and final shelf

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,11 @@
 import "./Home.css";
 
 function Home() {
-  return <section id="home"></section>;
+  return <section id="home">
+    <p>Just a quick test</p>
+  </section>
+
+  ;
 }
 
 export { Home };
