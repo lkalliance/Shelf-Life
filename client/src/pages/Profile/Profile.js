ffea72c4b37341770321c536d92c0ea0576d92b7
Safia Ali <ali.swaphiyah@gmail.com>
2023-05-24 16:09:42 -0500

    added image to booklist

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -44,6 +44,9 @@ function Profile() {
               key={book.bookId}
               className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
             >
+              {book.image ? (
+                <img src={book.image} alt={`The cover for ${book.title}`} />
+              ) : null}
               <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
                 {book.title}
               </h5>
