033d70c9a5480d4314d0fd30f1a3b3b85c86b353
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-05-24 13:11:06 -0500

    fixing Atlas connection

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ const authLink = setContext((_, { headers }) => {
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
 
-  uri: "http://localhost:3001/graphql",
+  uri: "http://:3001/graphql",
   cache: new InMemoryCache({
     addTypename: false,
   }),
