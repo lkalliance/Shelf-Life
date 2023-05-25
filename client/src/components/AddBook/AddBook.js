ea7589c075ab8d0e77bb1ace75ca335618af1993
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-05-24 23:55:58 -0500

    Re-split out card with Safia's changes, made the selection box close on submit, not just the search modal.

--- client/src/components/AddBook/AddBook.js
+++ client/src/components/AddBook/AddBook.js
@@ -135,6 +135,7 @@ function AddBook() {
     setbCase(newCase);
     setItems(convert(newCase));
     handleClose();
+    handleSelectionClose();
   };
 
   return (
@@ -408,7 +409,14 @@ function AddBook() {
                           className="label block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                         >
                           {" "}
-                          Rating <div className="star-container">{selected.rating >= 1 && <Star />} {selected.rating >= 2 && <Star />} {selected.rating >= 3 && <Star />} {selected.rating >= 4 && <Star />} {selected.rating >= 5 && <Star />}</div>
+                          Rating{" "}
+                          <div className="star-container">
+                            {selected.rating >= 1 && <Star />}{" "}
+                            {selected.rating >= 2 && <Star />}{" "}
+                            {selected.rating >= 3 && <Star />}{" "}
+                            {selected.rating >= 4 && <Star />}{" "}
+                            {selected.rating >= 5 && <Star />}
+                          </div>
                           <input
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             type="number"
