483a947f3096946861f05593311b73bc440aded3
Safia Ali <ali.swaphiyah@gmail.com>
2023-05-24 23:35:11 -0500

    resized search

--- client/src/components/AddBook/AddBook.js
+++ client/src/components/AddBook/AddBook.js
@@ -159,7 +159,7 @@ function AddBook() {
             : `fixed top-0 left-0 right-0 z-50 hidden mx-auto modalClass  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full`
         }
       >
-        <div className=" mx-auto relative w-full max-w-xl max-h-full">
+        <div className=" mx-auto relative w-full max-w-md max-h-full">
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <button
               onClick={() => {
