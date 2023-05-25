57e56064aa6e9b6da198535e280bb3c782fe22ff
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-05-24 23:27:51 -0500

    Broke out card component from Profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -3,6 +3,7 @@ import { useRecoilState } from "recoil";
 import { useQuery } from "@apollo/client";
 import { QUERY_ME, QUERY_BOOKCASE } from "../../utils/queries";
 import { convert } from "../../utils/dragUtils";
+import { Card } from "../../components";
 
 import {
   userBooksAtom,
@@ -39,33 +40,7 @@ function Profile() {
     <section id="profile">
       <div className="grid grid-cols-1 content-center">
         {books.bookList.map((book, index) => {
-          return (
-            <div
-              key={book.bookId}
-              className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-            >
-              {book.image ? (
-                <img src={book.image} alt={`The cover for ${book.title}`} />
-              ) : null}
-              <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {book.title}
-              </h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                {" "}
-                {book.authors.map((author, index) => {
-                  return <span key={index}>{author}</span>;
-                })}
-              </p>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                {book.rating === 1
-                  ? `${book.rating} star`
-                  : `${book.rating} stars`}
-              </p>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                {book.comment}{" "}
-              </p>
-            </div>
-          );
+          return <Card book={book} />;
         })}
       </div>
     </section>
