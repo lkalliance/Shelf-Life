251a480f5c654f17c1262f4d8cb85ed937747c0e
lkalliance <119076402+lkalliance@users.noreply.github.com>
2024-07-01 15:22:04 -0500

    lengthened login timeout to 30 days

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const secret = "mysecretssshhhhhhh";
-const expiration = "24h";
+const expiration = "30d";
 
 module.exports = {
   authMiddleware: function ({ req }) {
